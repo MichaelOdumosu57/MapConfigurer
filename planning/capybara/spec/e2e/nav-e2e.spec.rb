1d4fc4b9b54d93c90bee1374b0a4311dcea5e60c
Michael Odumosu <michaelodumosu57@gmail.com>
2020-01-31 11:00:08 -0500

    percy visual regression testing

--- planning/capybara/spec/e2e/nav-e2e.spec.rb
+++ planning/capybara/spec/e2e/nav-e2e.spec.rb
@@ -6,6 +6,8 @@ require 'capybara/rspec'
 require 'capybara/rspec/matcher_proxies'
 require 'rspec/expectations'
 require 'rails_helper'
+require 'percy'
+
 
 # $:.each do |d|
 #     p d 
@@ -26,66 +28,104 @@ module TestMod
   
   def TestMod.startTest  
     # @javascript
-    RSpec.feature "navigation stuff" do
-      scenario "Go to home page" do
+    # RSpec.feature "navigation stuff" do
+    #   scenario "Go to home page" do
+    #     visit '/'
+    #     expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
+    #     expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject4]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject1]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject0]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject2]';
+    #     expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+    #   end
+    #   scenario "head to about page" do
+    #     visit '/'
+    #     # puts page.has_selector? '#n_a_v_i_g_a_t_i_o_n_aboutLink'
+    #     elem = first "#n_a_v_i_g_a_t_i_o_n_aboutLink" 
+    #     elem.select_option
+    #     expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
+    #     expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject3]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject5]';
+    #     expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+    #     #sleep 9000
+    #   end
+    #   scenario "head to services page" do
+    #     visit '/'
+    #     elem = first "#n_a_v_i_g_a_t_i_o_n_servicesLink" 
+    #     elem.select_option
+    #     expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
+    #     expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject0]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject3]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject4]';
+    #     expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+    #   end    
+    #   scenario "head to projects page" do
+    #     visit '/'
+    #     elem = first "#n_a_v_i_g_a_t_i_o_n_projectsLink" 
+    #     elem.select_option
+    #     expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
+    #     expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject1]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject6]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject7]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject8]';
+    #     expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+    #   end     
+    #   scenario "head to blog page" do
+    #     visit '/'
+    #     elem = first "#n_a_v_i_g_a_t_i_o_n_blogLink" 
+    #     elem.select_option
+    #     expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
+    #     expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject5]';
+    #     expect(page).to have_selector 'app-blog[ng-reflect-blog-t-v=blogCO0]';
+    #     expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+    #   end  
+    #   scenario "head to contact page" do
+    #     visit '/'
+    #     elem = first "#n_a_v_i_g_a_t_i_o_n_contactLink" 
+    #     elem.select_option
+    #     expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
+    #     expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject2]';
+    #     expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject9]';
+    #     expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+    #   end                      
+    # end
+
+    RSpec.feature "visual regression" do
+      scenario "take a snapshot of the homepage" do
         visit '/'
-        expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
-        expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject4]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject1]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject0]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject2]';
-        expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
+        Percy.snapshot page, { :name => 'windsor homepage' }
       end
-      scenario "head to about page" do
+      scenario "take a snapshot of the about page" do
         visit '/'
-        # puts page.has_selector? '#n_a_v_i_g_a_t_i_o_n_aboutLink'
         elem = first "#n_a_v_i_g_a_t_i_o_n_aboutLink" 
-        elem.select_option
-        expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
-        expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject3]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject5]';
-        expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
-        #sleep 9000
-      end
-      scenario "head to services page" do
+        elem.select_option        
+        Percy.snapshot page, { :name => 'windsor about page' }
+      end   
+      scenario "take a snapshot of the services page" do
         visit '/'
         elem = first "#n_a_v_i_g_a_t_i_o_n_servicesLink" 
-        elem.select_option
-        expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
-        expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject0]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject3]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject4]';
-        expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
-      end    
-      scenario "head to projects page" do
+        elem.select_option        
+        Percy.snapshot page, { :name => 'windsor services page' }
+      end
+      scenario "take a snapshot of the projects page" do
         visit '/'
         elem = first "#n_a_v_i_g_a_t_i_o_n_projectsLink" 
-        elem.select_option
-        expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
-        expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject1]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject6]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject7]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject8]';
-        expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
-      end     
-      scenario "head to blog page" do
+        elem.select_option        
+        Percy.snapshot page, { :name => 'windsor projects page' }
+        
+      end
+      scenario "take a snapshot of the blog page" do
         visit '/'
         elem = first "#n_a_v_i_g_a_t_i_o_n_blogLink" 
-        elem.select_option
-        expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
-        expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject5]';
-        expect(page).to have_selector 'app-blog[ng-reflect-blog-t-v=blogCO0]';
-        expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
-      end  
-      scenario "head to contact page" do
+        elem.select_option        
+        Percy.snapshot page, { :name => 'windsor blog page' }
+      end
+      scenario "take a snapshot of the contact page" do
         visit '/'
         elem = first "#n_a_v_i_g_a_t_i_o_n_contactLink" 
-        elem.select_option
-        expect(page).to have_selector 'app-navigation[ng-reflect-navigation-template-variable=navigationComponentObject0]';
-        expect(page).to have_selector 'app-overlay[ng-reflect-overlay-template-variable=overlayComponentObject2]';
-        expect(page).to have_selector 'app-words[ng-reflect-words-template-variable=wordsComponentObject9]';
-        expect(page).to have_selector 'app-footer[ng-reflect-footer-template-variable=footerComponentObject0]';
-      end                      
+        elem.select_option        
+        Percy.snapshot page, { :name => 'windsor  contact page' }
+      end               
     end
 =begin  
     describe "Create place scenario" do
